ed340b5658655dd7dcd3fdc04bd13d97c053eee2
HAKing <haking.design@gmail.com>
2025-12-07 19:50:04 -0600

    work toward content image fix

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import './assets/app.css'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -21,74 +22,3 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
   <RouterView style="max-height: 70vh; " />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 50vh;
-  background-color: #00000000;
-}
-header .wrapper {
-  background-color: #00000000;
-}
-.logo {
-  display: block;
-  width:75px; 
-  height:75px;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 1rem;
-  text-align: center;
-  margin-top: 0rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    width:150px; 
-    height:150px;
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1.2rem;
-
-    padding: 1rem 0;
-    margin-top: 2rem;
-  }
-}
-</style>
--- src/world/components/scene.js
+++ src/world/components/scene.js
@@ -3,7 +3,7 @@ import { Color, Scene, TextureLoader } from 'three';
 function createScene(color) {
  const scene = new Scene();
  
- scene.background = new TextureLoader().load('./src/assets/Space WIP_blend2_2 stars.png');
+ scene.background = new TextureLoader().load('./src/assets/Space_WIP_blend2_2_stars.png');
  
  return scene;
 }
